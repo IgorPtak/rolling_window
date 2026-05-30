bad80f6f56aca4e92d8dce4fc12d14b72fb4e78a
ptak <ptakigor@icloud.com>
2026-05-30 12:17:58 +0200

    fix: implementation

--- src_core/TwoHeapMedian.cpp
+++ src_core/TwoHeapMedian.cpp
@@ -1,27 +1,31 @@
 #include "TwoHeapMedian.hpp"
-#include <iterator>
 #include <limits>
 #include <stdexcept>
 
 TwoHeapMedian::TwoHeapMedian(std::size_t size) : window_size_(size) {
-  if (size <= 0) {
+  if (size == 0)
     throw std::invalid_argument("Window length must be greater than 0");
-  }
 }
 
 void TwoHeapMedian::clean_lower() const {
-  while (!lower.empty() && pending.count(lower.top()) > 0) {
-    pending[lower.top()]--;
+  while (!lower.empty()) {
+    auto it = pending.find(lower.top());
+    if (it == pending.end())
+      break;
     lower.pop();
-    lower_eff--;
+    if (--it->second == 0)
+      pending.erase(it);
   }
 }
 
 void TwoHeapMedian::clean_upper() const {
-  while (!upper.empty() && pending.count(upper.top()) > 0) {
-    pending[upper.top()]--;
+  while (!upper.empty()) {
+    auto it = pending.find(upper.top());
+    if (it == pending.end())
+      break;
     upper.pop();
-    upper_eff--;
+    if (--it->second == 0)
+      pending.erase(it);
   }
 }
 
@@ -30,65 +34,60 @@ std::size_t TwoHeapMedian::current_size_impl() const {
 }
 
 double TwoHeapMedian::get_value_impl() const {
-  clean_lower();
-  clean_upper();
-
-  if (lower.empty() && upper.empty()) {
+  if (lower_eff == 0 && upper_eff == 0)
     return std::numeric_limits<double>::quiet_NaN();
-  }
-
-  if (lower_eff > upper_eff) {
+  clean_lower();
+  if ((lower_eff + upper_eff) % 2 == 1)
     return lower.top();
-  } else {
-    return (lower.top() + upper.top()) / 2;
-  }
+  clean_upper();
+  return (lower.top() + upper.top()) / 2.0;
 }
 
 void TwoHeapMedian::evict(double oldest) {
-  pending[oldest]++;
-
   clean_lower();
-
-  if (!lower.empty() && oldest <= lower.top()) {
+  const bool in_lower = !lower.empty() && oldest <= lower.top();
+  pending[oldest]++;
+  if (in_lower) {
     lower_eff--;
+    if (lower_eff < upper_eff) {
+      clean_upper();
+      lower.push(upper.top());
+      upper.pop();
+      lower_eff++;
+      upper_eff--;
+    }
   } else {
     upper_eff--;
-  }
-
-  if (lower_eff > upper_eff + 1) {
-    clean_lower();
-    upper.push(lower.top());
-    lower.pop();
-    lower_eff--;
-    upper_eff++;
-  } else if (upper_eff > lower_eff) {
-    clean_upper();
-    lower.push(upper.top());
-    upper.pop();
-    upper_eff--;
-    lower_eff++;
+    if (lower_eff > upper_eff + 1) {
+      upper.push(lower.top());
+      lower.pop();
+      upper_eff++;
+      lower_eff--;
+    }
   }
 }
 
 void TwoHeapMedian::insert_value(double x) {
+  clean_lower();
   if (lower.empty() || x <= lower.top()) {
     lower.push(x);
     lower_eff++;
+    if (lower_eff > upper_eff + 1) {
+      upper.push(lower.top());
+      lower.pop();
+      lower_eff--;
+      upper_eff++;
+    }
   } else {
     upper.push(x);
     upper_eff++;
-  }
-
-  if (lower_eff > upper_eff + 1) {
-    upper.push(lower.top());
-    lower.pop();
-    lower_eff--;
-    upper_eff++;
-  } else if (upper_eff > lower_eff) {
-    lower.push(upper.top());
-    upper.pop();
-    upper_eff--;
-    lower_eff++;
+    if (lower_eff < upper_eff) {
+      clean_upper();
+      lower.push(upper.top());
+      upper.pop();
+      upper_eff--;
+      lower_eff++;
+    }
   }
 }
 
@@ -96,9 +95,8 @@ void TwoHeapMedian::update_impl(double x) {
   if (history_.size() >= window_size_) {
     double oldest = history_.front();
     history_.pop();
-    if (!std::isnan(oldest)) {
+    if (!std::isnan(oldest))
       evict(oldest);
-    }
   }
   insert_value(x);
   history_.push(x);
@@ -108,9 +106,8 @@ void TwoHeapMedian::skip_impl() {
   if (history_.size() >= window_size_) {
     double oldest = history_.front();
     history_.pop();
-    if (!std::isnan(oldest)) {
+    if (!std::isnan(oldest))
       evict(oldest);
-    }
   }
   history_.push(std::numeric_limits<double>::quiet_NaN());
 }
